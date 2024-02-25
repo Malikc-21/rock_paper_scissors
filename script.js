3d0a18442e5d75f47ca907950f701e846a8b4f9c
malikc21 <malikc543@gmail.com>
2024-02-25 12:15:58 -0600

    implemted function to play 5 rounds as well as create user interface

--- script.js
+++ script.js
@@ -44,3 +44,94 @@ function playround(playerPick, computerPick){
     
     
 }
+
+let userScore = 0;
+let computerScore = 0;
+let win;
+let lose;
+let tie;
+let rounds = 0;
+let gameover = false
+let result;
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+    playerPick = button.id;
+    computerPick = getComputerChoice()
+    rounds = ++rounds;
+    
+    (playround(playerPick, computerPick))
+    
+    
+    const container = document.querySelector('#container')
+
+    const yourScore = document.createElement('div')
+    yourScore.textContent = "Your score is " + userScore + " - you chose " + playerPick
+    container.appendChild(yourScore)
+
+    
+    const compScore = document.createElement('div')
+    compScore.textContent = "Computer score is " + computerScore  + " - they chose " + computerPick
+    container.appendChild(compScore)
+
+    const round = document.createElement('div')
+    round.textContent = "round " + rounds 
+    container.appendChild(round)
+
+    if (rounds === 5) {
+        gameover = true
+
+    }
+    
+    if (gameover == true) {
+
+    
+        if (computerScore < userScore) {
+
+            gameover = false
+            rounds = 0
+            userScore = 0
+            computerScore = 0
+            result = 'You win!'
+
+            const winner = document.createElement('h1')
+            winner.textContent = result
+            container.appendChild(winner)
+
+            }
+            else if (computerScore > userScore) {
+            
+            gameover = false
+
+            rounds = 0
+            rounds = 0
+            userScore = 0
+            computerScore = 0
+
+            result = 'the computer won'
+
+            const winner = document.createElement('h1')
+            winner.textContent = result
+            container.appendChild(winner)
+
+            }
+            else if (computerScore === userScore) {
+            
+            gameover = false
+
+            rounds = 0
+            rounds = 0
+            userScore = 0
+            computerScore = 0
+
+            result = 'It is a tie'
+
+            const winner = document.createElement('h1')
+            winner.textContent = result
+            container.appendChild(winner)
+
+            } 
+    }
+    });
+});
